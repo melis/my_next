0215ad5b079e17d7a78f911707753f8b7fde6f8f
Melis <59087686+melis@users.noreply.github.com>
2022-03-23 03:48:13 +0600

    Update store.js

--- store/store.js
+++ store/store.js
@@ -1,8 +1,14 @@
-import { createStore, applyMiddleware, combineReducers  } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose  } from 'redux';
 import posts from './post/postReducer'
 import reduxThunk from 'redux-thunk';
 
 const rootReduser = combineReducers({ posts });
- const store = createStore(rootReduser,applyMiddleware(reduxThunk));
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+    const store = createStore(rootReduser,composeEnhancers(applyMiddleware(reduxThunk)));
 
  export default store
