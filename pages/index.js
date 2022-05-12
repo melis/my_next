8d9b1339fb2525ed2920f45bfa1142b8a1924fb4
Melis <59087686+melis@users.noreply.github.com>
2022-05-12 18:42:24 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,34 @@
-import axios from 'axios'
-import Link from 'next/link'
+import axios from "axios";
+import Link from "next/link";
 
 export async function getStaticProps() {
- 
-   const {data} = await axios.get('https://jsonplaceholder.typicode.com/posts')
-  
-   if (!data) {
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+
+  if (!data) {
     return {
       notFound: true,
-    }
+    };
   }
 
   return {
-    props: { data, time: new Date().toString() }, 
+    props: { data, time: new Date().toString() },
     revalidate: 10, // In seconds
-  }
+  };
 }
 
-const Home=({data, time})=> {
+const Home = ({ data, time }) => {
   return (
-   <ul><li>{time}</li> {data.map(post=><li key = {post.id} style={{padding: '5px'}} >{post.id}  <Link href={`/post/${post.id}`} >{post.title}</Link></li>)}</ul>
-     )
-}
+    <ul>
+      <li>{time}</li>{" "}
+      {data.map((post) => (
+        <li key={post.id} style={{ padding: "5px" }}>
+          {post.id} <Link href={`/post/${post.id}`}>{post.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-export default Home
+export default Home;
